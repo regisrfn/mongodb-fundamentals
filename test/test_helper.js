9f72cbfcb3f6a1c7223cca5cce631c370ab30c90
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-05-31 14:40:28 -0300

    first commit

---
+++ test/test_helper.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/users_test',{ useNewUrlParser: true });
+
+mongoose.connection
+    .once('open', () => console.log("Good to go"))
+    .on('error' , (error) => {
+        console.warn("Error CONNECTION",error)
+    });

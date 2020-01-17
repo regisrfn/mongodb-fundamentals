fdd6d42673625ee6b48f5260887f27060e28237b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-01-17 11:21:42 -0200

    Created User database

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect(`${process.env.DATABASE_API}/users_test`,{ useNewUrlParser: true });
+//development mode
+//DATABASE_API = mongodb://localhost
+mongoose.connect(`${process.env.DATABASE_API}/users_test`, { useNewUrlParser: true })
 
 mongoose.connection
     .once('open', () => console.log("Good to go"))
-    .on('error' , (error) => {
-        console.warn("Error CONNECTION",error)
-    });             
+    .on('error', (error) => {
+        console.warn("Error CONNECTION", error)
+    })          

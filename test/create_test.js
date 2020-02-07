7a77b6b089aa18ef4e30e1f26f3337df50fea97e
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-02-07 16:43:59 -0200

    Saving an user

--- test/create_test.js
+++ test/create_test.js
@@ -1,5 +1,9 @@
+const assert = require('assert')
+const User = require('../src/user')
+
 describe('Creating records', () => {
     it('saves a user', () => {
-        assert(1 + 1 === 2)
+        const joe = new User({name: 'Joe'})
+        joe.save()
     })
 })

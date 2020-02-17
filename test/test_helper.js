1182da2291c18c15980005f9e814377e6c0104ff
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-02-17 18:38:39 -0300

    test connection

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-//development mode
-//DATABASE_API = mongodb://localhost
-mongoose.connect(`${process.env.DATABASE_API}/users_test`, { useNewUrlParser: true })
+before((done) => {
+    //development mode
+    //DATABASE_API = mongodb://localhost
+    mongoose.connect(`${process.env.DATABASE_API}/users_test`, { useNewUrlParser: true })
 
-mongoose.connection
-    .once('open', () => {})
-    .on('error', (error) => {
-        console.warn("Error CONNECTION", error)
-    })
-    
+    mongoose.connection
+        .once('open', () => { done()})
+        .on('error', (error) => {
+            console.warn("Error CONNECTION", error)
+        })
+})
+
 beforeEach((done) => {
     mongoose.connection.collections.users.drop()
-    .then(() => {
-        done()
-    })
+        .then(() => {
+            done()
+        })
 })

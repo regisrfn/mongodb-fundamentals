7c809138e6f5971ac4e2e0d51f43d6602fb0cca8
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-02-27 13:59:52 -0300

    Setting up reading test

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,15 +2,20 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe("Reading users out of the database", () => {
+    const joe = new User({ name: 'Joe' })
     beforeEach(done => {
-        const joe = new User({ name: 'Joe' })
         joe.save()
         .then (() => done())
     })
     it('finds all users with a name of joe', (done) => {
         User.find({ name: 'Joe' })
             .then((users) => {
-                console.log(users)
+                assert(users[0]._id.toString() === joe._id.toString())
+                done()
+            })
+            .catch(error => {
+                console.log(error)
+                assert(false)
                 done()
             })
     })

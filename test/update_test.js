b9e5ec5aedcb2874ae6dd21b01e888ee566c3658
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-08-20 12:30:12 -0300

    Testing update

--- test/update_test.js
+++ test/update_test.js
@@ -1,6 +1,17 @@
 const assert = require('assert')
 const User = require('../src/user')
 
+function assertName(saveOperation, done) {
+    saveOperation
+        .then(() => User.find())
+        .then(users => {
+            assert(users.length === 1)
+            assert(users[0].name === 'Alex')
+            done()
+        })
+        .catch(error => done(error))
+}
+
 describe('Updating records', () => {
     var joe = null
 
@@ -13,12 +24,10 @@ describe('Updating records', () => {
 
     it('instance type using set and save', (done) => {
         joe.set('name', 'Alex')
-        joe.save()
-            .then(() => User.find())
-            .then(users => {
-                assert(users.length === 1)
-                assert(users[0].name === 'Alex')
-                done()
-            })
+        assertName(joe.save(), done)
+    })
+
+    it('A model instance can update', (done) => {
+        assertName(joe.updateOne({ name: 'Alex' }), done)
     })
 })

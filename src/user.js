4994a994da0b695adecac5af5b2396f955dc74cf
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-11-10 16:44:48 -0300

    Validating username

--- src/user.js
+++ src/user.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema
 const UserSchema = new Schema({
     name: {
         type: String,
+        validate: {
+            validator: (name) => name.length > 2,
+            message: "Name too short"
+        },
         required: [true, 'Name is required']
     },
     postCount: Number
--- test/validation_test.js
+++ test/validation_test.js
@@ -1,12 +1,31 @@
-const assert =  require('assert')
+const assert = require('assert')
 const User = require('../src/user')
 
-describe ('Validating records', () => {
-    it ('username required', () => {
-        const user = new User({name: undefined})
+describe('Validating records', () => {
+    it('username required', () => {
+        const user = new User({ name: undefined })
         const validationResult = user.validateSync()
-        const {message} = validationResult.errors['name']
+        const { message } = validationResult.errors['name']
 
         assert(message === 'Name is required')
-    }) 
+    })
+
+    it('username size', () => {
+        const user = new User({ name: 'Al' })
+        const validationResult = user.validateSync()
+        const { message } = validationResult.errors['name']
+
+        assert(message === 'Name too short')
+    })
+
+    it('saving username invalid', (done) => {
+        const user = new User({ name: 'Al' })
+        user.save()
+            .then (() =>  done('error - user shound\'t be saved'))
+            .catch(err => {
+                const {message} = err.errors['name']
+                assert(message === 'Name too short')
+                done ()
+            })
+    })
 })

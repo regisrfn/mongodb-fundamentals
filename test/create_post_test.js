c34b3ed8bddca5e13c7c0c9256e78a4cb35b4936
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-01-20 16:17:36 -0300

    add subdocument

--- test/create_post_test.js
+++ test/create_post_test.js
@@ -1,5 +1,7 @@
 const assert = require('assert')
 const User = require('../src/user')
+const Post = require('../src/post')
+
 
 describe('Creating post', () => {
     it('saves a post', async () => {
@@ -10,7 +12,27 @@ describe('Creating post', () => {
         await joe.save()
         assert(!joe.isNew)
 
-        const user = await User.findOne({name:"Joe"})
+        const user = await User.findOne({ name: "Joe" })
         assert(user.posts[0].title === 'PostTitle')
+    })
+
+    it('add subdocuments to an existing record', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []
+        })
+        joe.save()
+            .then(() => User.findOne({name: "Joe"}))
+            .then(user => {
+                user.posts.push({title: "New Post"})
+                return user.save();
+            })
+            .then((user) => {
+                assert.strictEqual(joe._id.toString(),user._id.toString())
+                done()
+            })
+            .catch( error => {
+                done(error)
+            })
     })
 })

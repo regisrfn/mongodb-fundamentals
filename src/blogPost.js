b8282ed551c688185b7c1850e068721a7a5f6d40
Regis Rufino Rodrigues <regis.rfnrodrigues@gmail.com>
2021-07-22 12:19:06 -0300

    ref comment into blog_post

--- src/blogPost.js
+++ src/blogPost.js
@@ -4,4 +4,11 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
     title: String,
     content: String,
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    }]
 });
+
+const BlogPost = mongoose.model('blog_post', BlogPostSchema);
+module.exports = BlogPost;
